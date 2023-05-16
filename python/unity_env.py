ae6eb2f91e4463456d81d515c4449e654ae7d20c
Ribault Nicolas <nicolas.ribault@ecole.ensicaen.fr>
2023-05-16 18:50:53 +0200

    missing type hint

--- python/unity_env.py
+++ python/unity_env.py
@@ -8,7 +8,7 @@ class UnityEnv(GymEnv):
     def __init__(self) -> None:
         super().__init__()
 
-    def reset():
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None,) -> tuple[ObsType, dict[str, Any]]:
         pass
 
     def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
